80999b0fb0e5162f9640432635d5b177d6a81380
larissaer <larissaeneidarodrigues@gmail.com>
2020-09-26 11:20:24 -0600

    not displaying colums right yet

---
+++ lesson2/scripts/main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', (event)=>{
+    const lu = document.querySelector('#lastupdated');
+    lu.textContent=document.lastModified;
+
+    const copyrightyear =document.querySelector('#copyrightyear');
+    copyrightyear.textContent=new Date().getFullYear();
+    
+} )
