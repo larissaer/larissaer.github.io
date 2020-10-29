1a0fcc6490d30a9e61e6cfb11faa9360d78a484c
larissaer <larissaeneidarodrigues@gmail.com>
2020-10-28 18:14:19 -0600

    finish fixes

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,16 +1,14 @@
-window.addEventListener("load", (event) => {
+window.addEventListener('load', (event)=>{
+    var text;
+    var temp = parseFloat(document.getElementById("currentTemperature").textContent);
+    var speed = parseFloat(document.getElementById("windSpeed").textContent);
 
-var result;
-var temp = parseFloat(document.getElementById("currentTemp").textContent);
-var speed = parseFloat(document.getElementById("windSpeed").textContent);
-
-if (speed >= 3 && temp <= 50){
-    text = (parseInt(35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16))) + ("℉"));
-} else {
-    text = "N/A"
-}
-
-const windChill = document.querySelector("#windChill");
-windChill.textContent = result;
+    if (speed >= 3 && temp <= 50){
+        text = (parseInt(35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16))) + ("℉"));
+    } else {
+        text = "N/A"
+    }
+    const windChill = document.querySelector("#windChill");
+    windChill.textContent = text;
 
 })
