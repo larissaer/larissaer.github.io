58613f9df4ef69e00ed28aba3800371d1f865b16
larissaer <larissaeneidarodrigues@gmail.com>
2020-11-18 19:49:16 -0700

    start lesson 10

---
+++ lesson10/js/preston.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", (event) => {
+    //date
+    const lastUpdate = document.querySelector("#lastupdate");
+    lastUpdate.textContent = document.lastModified;
+
+    const year = document.querySelector("#year");
+    year.textContent = new Date().getFullYear();
+
+    // toggle on menu
+
+    const hambutton = document.querySelector('.ham');
+
+    const mainnav = document.querySelector('.navigation')
+
+    hambutton.addEventListener('click', () => {
+        mainnav.classList.toggle('responsive')
+    }, false);
+
+
+    //top banner
+
+    var ad = document.getElementById("topBanner")
+    if (dateObject.getDay() == 5) {
+        ad.style.display = "block";
+    } else {
+        ad.style.display = "none";
+    }
+
+})
